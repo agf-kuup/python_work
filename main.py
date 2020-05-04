7a4b8fae4d2eac9ab6e7e2a25f9d058bd239c1db
coder.generico <agf0710@gmail.com>
2020-05-04 15:38:04 -0500

    Advices are shown in popups

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+import os                       # To create things in the computer.
+import simplejson as json       # To edit content in file data.
+import subprocess               # To open subprocesses in computer.
+import sys
+import time                     # To avoid running everything at the same time.
+import webbrowser               # To open the webbrowser.
+import PySimpleGUI as sg
+
 # Script to set up everything in the computer when it starts.
 # First the program will open the chrome browser to open all 
 # the tabs that are used frequently by me.
@@ -5,12 +13,8 @@
 # 
 # After that it opens the programs I usually open first.
 #  
-import os                       # To create things in the computer.
-import simplejson as json       # To edit content in file data.
-import subprocess               # To open subprocesses in computer.
-import sys
-import time                     # To avoid running everything at the same time.
-import webbrowser               # To open the webbrowser.
+
+sg.theme('LightGray1')	# Add a touch of color
 
 # Code to create data where any information involved to this project will be saved.
 path = os.getcwd()
@@ -21,17 +25,21 @@ name_new_dir=new_dir.split('\\')[-1]
 try:
     os.mkdir(new_dir)
 except OSError:
-    print ("Creation of the directory \"{}\" failed. The directory already exists".format(name_new_dir))
+    message="Creation of the directory \"{}\" failed.\nThe directory already exists".format(name_new_dir)
 else:
-    print ("Successfully created the directory {} ".format(name_new_dir))
+    message="Successfully created the directory {} ".format(name_new_dir)
+window = sg.PopupAutoClose(message,title="Creating data",auto_close_duration=3)
+
 
 if os.path.isfile("./data/data.json") and os.stat('./data/data.json').st_size!=0:
-    print("Reading file...")
+    msg="Reading file...\n"
     data_file=open('./data/data.json','r+')
     d=json.loads(data_file.read())
+    msg+='Actual content of file is:\n'
+    msg+=''.join(c for c in json.dumps(d, sort_keys=True, indent=4 * ' ') if not c in ['[',']','{','}',',','"'])
     data_file.close()
 else:
-    print("Creating new data file...")
+    msg="Creating new data file...\n"
     data_file=open('./data/data.json','w+')
     data_file.seek(0)
     d={'webs':[ 'https://github.com/',
@@ -42,27 +50,33 @@ else:
                 'C:/Users/anton/AppData/Roaming/Telegram Desktop/Telegram.exe',
                 'C:/Users/anton/AppData/Local/Programs/Microsoft VS Code/Code.exe'
                 ]}
+    msg+="The content of data file will be:\n"
+    msg+=''.join(c for c in json.dumps(d, sort_keys=True, indent=4 * ' ') if not c in ['[',']','{','}',',','"'])
     data_file.write(json.dumps(d))
-    print("Updating data...\n")
+    msg+="This content can be edited in data/data.json reading the readme\nfile."
     data_file.close()
+window = sg.PopupAutoClose(msg[:-2],title="Reading data",auto_close_duration=3)
+
+
 #######################################
 #Opening and running programs for starting day.
 # chrome.exe folder
 chromedir= 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s'
-print("\nActual webpages added are:")
 for i in d['webs']:
-    #webbrowser.get(chromedir).open(i)
-    print("    {}".format(i))
-print("\nActual programs added are:")
+    webbrowser.get(chromedir).open(i)
 for i in d['programs']:
-    #subprocess.Popen([i, '-new-tab'], stderr=subprocess.DEVNULL)
+    subprocess.Popen([i, '-new-tab'], stderr=subprocess.DEVNULL)
     time.sleep(1)
-    print("    {}".format(i))
 
-githubcheck=input("Do you want to check your github? Yes(y) / No(n)\n")
-if githubcheck=='y':
-    subprocess.call(["python", "pushed.py"])
-else:
-    print("""Closing...
-If you change your mind, you can run just pushed.py.""")
-    exit()
+gitmsg="Do you want to check your github?"
+
+popup=sg.popup_yes_no(gitmsg,title="Github")  # Shows Yes and No buttons
+while True:
+    if popup in (None,'No'):        # if user closed the window using X or clicked Quit button
+        last_msg="""Closing...\nIf you change your mind, you can run just pushed.py."""
+        window = sg.PopupAutoClose(last_msg,title="Closing",auto_close_duration=4)
+        break
+    else: 
+        subprocess.call(["python", "pushed.py"])
+        break
+exit()
