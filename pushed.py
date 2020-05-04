95aede4876141534220270785acb7b6b3aa8f734
coder.generico <agf0710@gmail.com>
2020-05-04 15:41:14 -0500

    popups for all updated except no internet process

--- pushed.py
+++ pushed.py
@@ -3,8 +3,9 @@ import requests                         # Ask for webpages.
 import os                               # Create things in the computer.
 import sys                              # Permit close if main wasn't run.
 import time                             # Sleep program until 15 minutes later.
-from win10toast import ToastNotifier    # Windows Notification
 import simplejson as json               # To edit content in file data.
+import PySimpleGUI as sg      
+from win10toast import ToastNotifier    # Windows Notification
 from datetime import date               # Get the date of today.
 from extra import getDate
 
@@ -20,16 +21,29 @@ d=json.loads(data_file.read())
 if 'github' in d.keys():
     username=d['github']
 else:
-    print('''In order to ask you, it's needed your profile's name.
+    msg='''In order to ask you, it's needed your profile's name.
 Enter to "Your Profile" and copy the part next to the last "/".
 For example, in\n
          https://github.com/repositorio-generico
-\nthe name is repositorio-generico\n''')
+\nthe name is repositorio-generico\n
+Paste the name of your repository.\nThis will be asked once if the username is saved in data.\n'''
+    layout = [ [sg.Txt(msg,key='msg')],      
+            [sg.In(key='username')],      
+            [sg.Button('Add', bind_return_key=True)]]
 
-    username=input("""Paste the name of your repository.\nThis will be asked once if the username is saved in data.\n""")
+    window = sg.Window('Username input', layout)      
+
+    while True:      
+        event, values = window.read()      
+
+        if event is not None:      
+            username = str(values['username'])      
+            window.close()
+            break
+        else:
+            break
     d['github']=username
     data_file.seek(0)
-    print("Your name was saved.")
     data_file.write(json.dumps(d))
 data_file.close()
 
@@ -38,11 +52,12 @@ flag=False
 r=requests.get("https://github.com/"+username)
 path = os.getcwd()
 if r.status_code!=200:
-    print("""Something is failing with your connection.
+    status_fail_msg="""Something is failing with your connection.
 This code will run but probably will fail if isn't in a folder with
 github repositories.
 If you have a folder where your repositories are saved,
-put this folder inside in order to find the last log you did""")
+put this folder inside in order to find the last log you did"""
+    sg.popup(status_fail_msg,title="Status")
     path=path.split(":")[1].replace('\\','/')[1:]
     output = subprocess.check_output(['bash.exe','-c', "cd ..; find . -name .git -type d -prune".format(path)])
     for i in output.decode("utf-8").split('\n')[:-1]:
@@ -53,22 +68,28 @@ put this folder inside in order to find the last log you did""")
                                     git log -1;".format(i[2:-4])])
         day=getDate(h)
         if today==day:
-            print("\nYou have done something today! :-)")
+            update="\nYou have done something today! :-)"
             flag=True
             break
-    print("\nYou haven't done anything today! :-(")
+    if flag==False:
+        update="\nYou haven't done anything today! :-("
 
 else:
     if 'fill="#ebedf0" data-count="0" data-date="{}"'.format(today) in r.content.decode("UTF-8"):
-        print("\nYou haven't done anything today! :-(")
+        update="\nYou haven't done anything today! :-("
     else:
         flag=True
-        print("\nYou have done something today! :-)")
+        update="\nYou have done something today! :-)"
+sg.PopupAutoClose(update,title="Status",auto_close_duration=5)
 
-if flag==True:
+if ":-)" in update:
+    sys.exit()
+
+if flag==False:
     while True:
-        X=input("Do you want me to remind you to push in your repos? Yes (y)/ No (n)\n")
-        if X=='n':
+        alert_msg="Do you want me to remind you to push in your repos?\n"
+        popup=sg.popup_yes_no(alert_msg,title="Alerts")
+        if popup=='No':
             sys.exit()
         else:
             while True:
@@ -86,4 +107,4 @@ if flag==True:
                                         icon_path="icon.ico",
                                         duration=10)
                     sys.exit()
-                time.sleep(60*15)
+                time.sleep(60*10)
